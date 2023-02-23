3009bdbfcd4e72f61877e728c8dbfa06a83690d5
Seun <samwhitedove@gmail.com>
2023-02-23 15:13:35 +0000

    hello updated

--- monnify/__init__.py
+++
@@ -1,5 +0,0 @@
-# from initialize_monnify import InitializeMonnifySDKTransaction
-
-# if __name__ == "module":    
-#     initializeSDK = InitializeMonnifySDKTransaction(contractCode="2465911000", apiKey="MK_TEST_FA1LZQ0KRW", secretKey="BY8RLEDX3EEUG1GJEDBHKKR5JGYAL3YF")
-#     initializeSDK.__str__()
